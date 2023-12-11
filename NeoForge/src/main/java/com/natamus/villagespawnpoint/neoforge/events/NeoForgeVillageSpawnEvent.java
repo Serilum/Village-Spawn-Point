57b6081c4f3010eb9a2845e7bf5369639bf632c5
Rick South <rick@natamus.com>
2023-12-11 23:49:57 +0100

    Village Spawn Point source code for 1.20.2.

---
+++ NeoForge/src/main/java/com/natamus/villagespawnpoint/neoforge/events/NeoForgeVillageSpawnEvent.java
@@ -0,0 +1,26 @@
+package com.natamus.villagespawnpoint.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.villagespawnpoint.events.VillageSpawnEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.ServerLevelData;
+import net.neoforged.neoforge.event.level.LevelEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeVillageSpawnEvent {
+	@SubscribeEvent(receiveCanceled = true)
+	public static void onWorldLoad(LevelEvent.CreateSpawnPosition e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		ServerLevel serverLevel = (ServerLevel)level;
+		if (VillageSpawnEvent.onWorldLoad(serverLevel, (ServerLevelData)serverLevel.getLevelData())) {
+			e.setCanceled(true);
+		}
+	}
+}
