6b37a77e4f24ff41cc47ef77e17cf5134f773943
Rick South <rick@natamus.com>
2024-04-28 01:21:00 +0200

    Village Spawn Point source code for 1.20.5.

---
+++ Forge/src/main/java/com/natamus/villagespawnpoint/ModForge.java
@@ -0,0 +1,34 @@
+package com.natamus.villagespawnpoint;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.villagespawnpoint.data.Constants;
+import com.natamus.villagespawnpoint.forge.events.ForgeVillageSpawnEvent;
+import com.natamus.villagespawnpoint.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+    	MinecraftForge.EVENT_BUS.register(new ForgeVillageSpawnEvent());
+	}
+
+	private static void setGlobalConstants() {
+		Constants.biomeSpawnPointLoaded = ModList.get().isLoaded("biomespawnpoint");
+	}
+}
