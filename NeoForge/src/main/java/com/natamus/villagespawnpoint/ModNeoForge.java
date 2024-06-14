56a6f2de2a64f13c4e0aba63fb5826edee597fb0
Rick South <rick@natamus.com>
2024-06-14 05:06:50 +0200

    Village Spawn Point source code for 1.21.0.

---
+++ NeoForge/src/main/java/com/natamus/villagespawnpoint/ModNeoForge.java
@@ -0,0 +1,32 @@
+package com.natamus.villagespawnpoint;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.villagespawnpoint.data.Constants;
+import com.natamus.villagespawnpoint.neoforge.events.NeoForgeVillageSpawnEvent;
+import com.natamus.villagespawnpoint.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge(IEventBus modEventBus) {
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeVillageSpawnEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+		Constants.biomeSpawnPointLoaded = ModList.get().isLoaded("biomespawnpoint");
+	}
+}
