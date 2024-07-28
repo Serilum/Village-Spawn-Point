926b5a9f5e9c765d78011c8f2ed49333cf53805f
Rick South <rick@natamus.com>
2024-07-28 07:10:40 +0200

    (v4.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/villagespawnpoint/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/villagespawnpoint/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.villagespawnpoint;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.villagespawnpoint.data.Constants;
 import com.natamus.villagespawnpoint.neoforge.events.NeoForgeVillageSpawnEvent;
 import com.natamus.villagespawnpoint.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
