fd69ddbd875f036af293234e3a6c62c8dec047f3
Rick South <rick@natamus.com>
2024-12-06 13:08:43 +0100

    Village Spawn Point source code for 1.21.4.

---
+++ Common/src/main/java/com/natamus/villagespawnpoint/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.villagespawnpoint.util;
+
+public class Reference {
+	public static final String MOD_ID = "villagespawnpoint";
+	public static final String NAME = "Village Spawn Point";
+	public static final String VERSION = "4.4";
+	public static final String ACCEPTED_VERSIONS = "[1.21.4]";
+}
