321577b7470c8188ff66f5dea3ad64acab8417da
Rick South <rick@natamus.com>
2026-06-18 13:09:04 +0200

    Updated Village Spawn Point to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/villagespawnpoint/util/Reference.java
+++ Common/src/main/java/com/natamus/villagespawnpoint/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "villagespawnpoint";
 	public static final String NAME = "Village Spawn Point";
 	public static final String VERSION = "4.7";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
